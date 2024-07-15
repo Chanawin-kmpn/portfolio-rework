221e358180d40590b8b8fa6da6e7076169f3a507
Upond <pond_chanawin@hotmail.com>
2024-07-15 18:19:52 +0700

    change contact paragraph sentence

--- components/AnimateMessage.tsx
+++ components/AnimateMessage.tsx
@@ -29,7 +29,7 @@ const AnimateMessage = () => {
 				className={`cloud-message ${isMessageVisible ? 'fade-in' : 'invisible'}`}
 			>
 				<p className="paragraph-regular">
-					If you are interested in me, please contact me.
+					If you are interested in me, please feel free to contact me.
 				</p>
 				<ScrollLink href="contact" isGradient={true}>
 					Contact me!
